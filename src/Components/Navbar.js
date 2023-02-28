ebd1153fd6eb318a449645d8546f733e5c838b92
Babuli Pallai <babulipalai6@gmail.com>
2023-02-28 20:08:36 +0530

    Accessing state in redux

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Navbar() {
+  const amount = useSelector((state) => state.amount)
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -34,7 +36,7 @@ export default function Navbar() {
             </ul>
             <div>
               <button className="btn btn-primary">
-                Your Balance: 18000
+                Your Balance: {amount}
               </button>
             </div>
           </div>
